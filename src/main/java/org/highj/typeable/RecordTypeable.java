0a6180fac80212ce95f166a11ea367d0b76cf990
Clinton Selke <clinuxrulz@gmail.com>
2016-07-24 11:39:23 +1000

    invmap for record and union

--- src/main/java/org/highj/typeable/RecordTypeable.java
+++ src/main/java/org/highj/typeable/RecordTypeable.java
@@ -2,12 +2,22 @@ package org.highj.typeable;
 
 import org.derive4j.hkt.__;
 import org.highj.data.tuple.T2;
+import org.highj.function.F1;
 
 public interface RecordTypeable<A> extends __<RecordTypeable.µ,A> {
     enum µ {}
     
     <F> __<F,A> run(InvariantRecordTypeable<F> context);
     
+    default <B> RecordTypeable<B> invmap(F1<A,B> f, F1<B,A> g) {
+        return new RecordTypeable<B>() {
+            @Override
+            public <F> __<F, B> run(InvariantRecordTypeable<F> context) {
+                return context.invmap(f, g, RecordTypeable.this.run(context));
+            }
+        };
+    }
+
     static <A> RecordTypeable<A> singleton(String tag, Typeable<A> type) {
         return new RecordTypeable<A>() {
             @Override
--- src/main/java/org/highj/typeable/UnionTypeable.java
+++ src/main/java/org/highj/typeable/UnionTypeable.java
@@ -3,12 +3,22 @@ package org.highj.typeable;
 import org.derive4j.hkt.__;
 import org.highj.data.Either;
 import org.highj.data.Maybe;
+import org.highj.function.F1;
 
 public interface UnionTypeable<A> extends __<UnionTypeable.µ,A> {
     enum µ {}
     
     <F> __<F,A> run(InvariantUnionTypeable<F> context);
     
+    default <B> UnionTypeable<B> invmap(F1<A,B> f, F1<B,A> g) {
+        return new UnionTypeable<B>() {
+            @Override
+            public <F> __<F, B> run(InvariantUnionTypeable<F> context) {
+                return context.invmap(f, g, UnionTypeable.this.run(context));
+            }
+        };
+    }
+
     static <A> UnionTypeable<A> singleton(String tag, Maybe<Typeable<A>> typeOp) {
         return new UnionTypeable<A>() {
             @Override
